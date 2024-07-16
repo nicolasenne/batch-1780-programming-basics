c8bf67fd81dda0f8b0297a83f83e41571fa290ee
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2024-07-16 11:18:22 -0300

    Add files via upload

---
+++ basics.rb
@@ -0,0 +1,66 @@
+# puts "Le Wagon"
+# puts 10
+
+# motivation = 10
+# puts "Go!" * motivation
+
+# puts "Le Wagon".class
+# puts "14".class
+# puts 10.class
+# puts 3.14.class
+# puts ["Le Wagon", 10, 3.14].class
+# puts true.class
+# puts false.class
+# puts (1...100).class
+
+# puts "le wagon".upcase
+# puts "le wagon" == "le wagon"
+# puts "two: #{1 + 1}"
+
+# puts "1984".class
+# puts "1984".to_i
+# puts "1984".to_i.class
+# puts "Le Wagon".to_i.class
+
+# puts 1 + 2
+# puts 2 * 4
+
+# puts 20.even?
+# puts 20.odd?
+
+# puts 1984.to_s.class
+
+# puts 3.9416.truncate
+# puts 2.5.round
+
+# puts ['Sponge Bob', 12, 3.14, "Hello"].size
+# p ["Nicolas", "Roberto", "Dudu"].sort
+# p [3, 9, 1, 4.12].sort
+
+# p %w[Nicolas Roberto Dudu]
+
+# p (1..20).to_a
+# p (1...20).to_a
+
+# p [].class
+
+# age = 19
+# puts "I am #{age} years old"
+
+# age = age + 1
+# puts "I am now #{age} years old"
+
+# city = "Paris"
+# population = 20000
+
+
+# # puts "#{city}, France"
+# # puts "#{population} is #{city} population"
+
+# city_details = "#{city}, France"
+# city_details
+
+# population = population + 1
+
+# puts population
+
---
+++ methods.rb
@@ -0,0 +1,49 @@
+require 'date'
+
+# puts "Hello John!"
+# puts "Hello Paul!"
+# puts "Hello Ringo!"
+
+# def say_hi(name)
+#   "Hi #{name}."
+# end
+
+# puts nicolas = say_hi("Nicolas")
+# puts say_hi("Dudu")
+
+# def say_hi(first_name, last_name)
+#   "Hi #{first_name} #{last_name}."
+# end
+
+# def say_hi(first_name, last_name)
+#   "Hi " + first_name + " " + last_name + "."
+# end
+
+# puts say_hi("Roberto", "Barros")
+
+# def max(x, y)
+#   if x > y
+#     return x
+#   else
+#     return y
+#   end
+# end
+
+# max_number = max(1, 2)
+# puts max_number
+
+# def tomorrow
+#   tomorrow_date = Date.today + 1
+#   tomorrow_date.strftime("%m/%d/%Y")
+# end
+
+# puts tomorrow
+
+# puts 20.even?
+# puts 20.odd?
+
+text = "Hello!"
+
+text.upcase!
+
+puts text
